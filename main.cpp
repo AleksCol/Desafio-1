b21c864135c6d032395d5c52a3ab93256d7f34da
Alejandro Arce <impersonality2k@gmail.com>
2026-09-15 16:46:08 -0500

    feat(tablero): declarar interfaz para reserva, lectura y escritura de fichas

--- main.cpp
+++ main.cpp
@@ -1,1 +1,6 @@
 #include <iostream>
+
+int main() {
+    std::cout << "Compilar" << std::endl;
+    return 0;
+}
--- tablero.h
+++ tablero.h
@@ -2,3 +2,8 @@
 #define TABLERO_H
 
 #endif // TABLERO_H
+
+int calcularBytesNecesarios(int filas, int columnas);
+unsigned char* crearTablero(int filas, int columnas);
+unsigned char leerFicha(unsigned char* tablero, int filas, int columnas, int fila, int columna);
+void escribirFicha(unsigned char* tablero, int filas, int columnas, int fila, int columna, unsigned char valor);
