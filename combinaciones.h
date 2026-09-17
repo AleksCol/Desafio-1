22a4fc7f686c6745d555acd73f8c3c468c3b51dd
Alejandro Arce <impersonality2k@gmail.com>
2026-09-17 16:43:17 -0500

    definiciones para el archio cpp

--- combinaciones.h
+++ combinaciones.h
@@ -1,4 +1,14 @@
 #ifndef COMBINACIONES_H
 #define COMBINACIONES_H
 
+#include "tablero.h"
+
+#define ficha_especial_jugable ficha_max
+
+bool marcarcombinaciones(unsigned char* tablero, int filas, int columnas, int* contadorfichaespecial);
+bool colapsaryrellenar(unsigned char* tablero, int filas, int columnas);
+
+int resolvercascada(unsigned char* tablero, int filas, int columnas, int* contadorfichaespecial)
+
+
 #endif // COMBINACIONES_H
