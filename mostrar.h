36f42fa1b2d4f9b1a51f95e1f23affa74f95b0e0
Alejandro Arce <impersonality2k@gmail.com>
2026-09-17 16:15:28 -0500

    funciones para poder ver en pantalla la informacion del tablero

--- mostrar.h
+++ mostrar.h
@@ -2,3 +2,13 @@
 #define MOSTRAR_H
 
 #endif // MOSTRAR_H
+
+
+//Estas funciones se van a usar para la presentacion
+//del tablero en la pantalla. Modulo de una sola lectura
+//en formato de fichas y en formato binario
+//
+
+void mostrartableroficha(unsigned char* tablero, int filas, int columnas );
+void mostrartablerobinario(unsigned char* tablero, int filas, int columnas);
+
