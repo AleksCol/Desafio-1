6a64bf1f2a2c04a63234529c63ab8be70e62397e
Alejandro Arce <impersonality2k@gmail.com>
2026-09-17 16:31:21 -0500

    Se añadieron definiciones en tablero.h, se acomodo el orden y en el cpp del tablero estoy mirando unos estados (redefiniciones o algo asi)

--- tablero.cpp
+++ tablero.cpp
@@ -1,1 +1,14 @@
-#include
+#include "Tablero.h"
+
+int calcularbytesnecesarios(int filas, int columnas, int ){
+    int totalbytes = filas = columnas = bits_por_ficha;
+    int totalbytes = totalbits / 8;
+    if (totalbits % 8 != 0) {
+        totalbytes++;
+
+    }
+
+    return totalbytes;
+
+
+}
--- tablero.h
+++ tablero.h
@@ -1,9 +1,20 @@
 #ifndef TABLERO_H
 #define TABLERO_H
 
-#endif // TABLERO_H
+//este es el dominio de los valores de 3 bit (0....7):6 fichas jugables + 2 estados internos.
+#define bits_por_ficha 3
+#define ficha_min 0
+#define ficha_max 5
+#define estado_especial 6
+#define estado_libre 7
 
 int calcularBytesNecesarios(int filas, int columnas);
 unsigned char* crearTablero(int filas, int columnas);
 unsigned char leerFicha(unsigned char* tablero, int filas, int columnas, int fila, int columna);
 void escribirFicha(unsigned char* tablero, int filas, int columnas, int fila, int columna, unsigned char valor);
+
+
+
+
+
+#endif // TABLERO_H
